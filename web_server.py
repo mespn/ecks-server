66674968006a34990c419acd10cf1e0b641df5ea
mespn <manuelen2002@gmail.com>
2023-11-01 21:01:05 -0500

    Basic server added

---
+++ web_server.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+import sys
+import os
+
+def handle_client(client):
+    pass
+
+def main():
+    SOCK_ADDR = ("", 8200)
+    PATH = os.path.abspath("ecks")
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_socket.bind(SOCK_ADDR)
+    server_socket.listen()
+
+    while True:
+        try:
+            (conn, addr) = server_socket.accept()
+            conn.settimeout(5)
+
+            thread = threading.Thread(target= handle_client, args=(conn, addr))
+            thread.start()
+
+        except KeyboardInterrupt:
+            sys.exit(0)
+        except:
+            print("Something happened.")
+
+if __name__ == '__main__':
+    main()
