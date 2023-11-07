93e875ed154689a1b5d7983962eebfef4d12feba
mespn <manuelen2002@gmail.com>
2023-11-07 15:55:58 -0600

    Implemented front-end page navigation

--- ecks/main.js
+++ ecks/main.js
@@ -1,26 +1,50 @@
-function doneBeenClicked () {
+function setUp () {
   // this is a closure, gets variables
   // from method it is in
   function loadedEventCallback () {
-    var ip = oReq.responseText;
-    var asObj = JSON.parse (ip);
-    var theDiv = document.getElementById ('fillHere');
-    ipAddresses.push (asObj['REMOTE_ADDR']);
-    //theDiv.innerText = "You are " + asObj['REMOTE_ADDR'];
-    var output = '<table>\n';
-    for (let i = 0; i < ipAddresses.length; i++) {
-      output += '<tr><td>' + ipAddresses[i] + '</td></tr>\n';
+    if (oReq.status == 401) {
+      var loginReq = new XMLHttpRequest ();
+      loginReq.addEventListener ('load', () => {
+        var theDiv = document.getElementById ('fill-here');
+        theDiv.innerHTML = loginReq.responseText;
+      });
+
+      loginReq.open ('GET', '/login.html');
+      loginReq.send ();
+    } else {
+      console.log ('trying to get tweets');
+      var mainPageReq = new XMLHttpRequest ();
+
+      mainPageReq.addEventListener ('load', () => {
+        var theDiv = document.getElementById ('fill-here');
+
+        theDiv.innerHTML = mainPageReq.responseText;
+
+        var tweetsReq = new XMLHttpRequest ();
+
+        tweetsReq.addEventListener ('load', showTweets);
+
+        tweetsReq.open ('GET', '/api/tweet');
+        tweetsReq.setRequestHeader ('Accept', 'application/json');
+        tweetsReq.send ();
+
+        function showTweets () {
+          var tweetList = JSON.parse (tweetsReq.responseText);
+          var tweetDiv = getElementById ('results');
+          tweetDiv.innerHTML = '<ul>';
+
+          tweetList.forEach (tweet => {
+            tweetDiv.innerHTML +=
+              '<li>' + tweet.author + ' said ' + tweet.content + '<li>';
+          });
+
+          tweetDiv.innerHTML += '</ul>';
+        }
+      });
     }
-    output += '</table>\n';
-    theDiv.innerHTML = output;
 
-    var otherDiv = document.getElementById ('topData');
-    otherDiv.innerText = '';
-  }
-
-  function loadedEventCallback2 () {
-    var theDiv = document.getElementById ('fillHere');
-    theDiv.innerText = 'I was done clicked';
+    mainPageReq.open ('GET', '/tweets.html');
+    mainPageReq.send ();
   }
 
   // basis from
@@ -28,9 +52,21 @@ function doneBeenClicked () {
   // probably will not work from localhost!
   var oReq = new XMLHttpRequest ();
   oReq.addEventListener ('load', loadedEventCallback);
-  // We want to get the IP address, but I don't want to talk too much about CORS!
-  oReq.open ('GET', '/~robg/cgi-bin/3010/get_ip.cgi');
-  // hey, we can do this, but don't have to (in this case)
-  oReq.setRequestHeader ('Accept', 'application/json');
+  oReq.open ('GET', '/api/tweets');
   oReq.send ();
+}
+
+function login () {
+  var loginReq = new XMLHttpRequest ();
+  var tbox = document.getElementById ('username-textbox');
+
+  loginReq.addEventListener ('load', setUp);
+  loginReq.open ('POST', '/api/login');
+  loginReq.send ('username=' + tbox.value);
+}
+
+function post_tweet () {
+  console.log ('Posting tweet: \n');
+  t_content = document.getElementById ('new-tweet').value;
+  console.log (t_content);
 }
